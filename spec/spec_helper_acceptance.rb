9ecf3c419dcb2b39354c2e8e97f2c8e84c7c5197
Robert Cohen <robert.cohen@anu.edu.au>
2023-06-16 21:48:31 +1000

    update apache/java requirements

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -3,8 +3,7 @@
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
 configure_beaker do |host|
-  install_module_from_forge_on(host, 'puppetlabs/apache', '>= 0 < 7.0.0')
-  install_module_from_forge_on(host, 'puppetlabs/java', '>= 0 < 8.0.0')
+  install_module_from_forge_on(host, 'puppetlabs/java')
 
   # testing dependencies
   scp_to(host, File.join(__dir__, 'acceptance', 'helpers', 'SSLPoke.class'), '/root/SSLPoke.class')
