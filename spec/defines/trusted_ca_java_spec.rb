ac6c0d1197216ecd57fee068a445857cf7c894f8
Ashley Gould <agould@ucop.edu>
2025-09-29 05:05:48 -0700

    Fix error in trusted_ca::java with ensure absent
    Fixes 96843d51db87daa54c3a8c10226f4b3136f63de6.

--- spec/defines/trusted_ca_java_spec.rb
+++ spec/defines/trusted_ca_java_spec.rb
@@ -47,6 +47,13 @@ describe 'trusted_ca::java' do
               with_command('keytool -import -noprompt -trustcacerts -alias mycert -file /tmp/mycert-trustedca -keystore /etc/alternatives/jre_1.7.0/lib/security/cacerts -storepass changeit')
           end
         end
+
+        context 'with filename' do
+          let(:local_params) { { content: 'abc', filename: '/path/to/file' } }
+
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to contain_exec('import /path/to/file to jks /etc/alternatives/jre_1.7.0/lib/security/cacerts').that_requires('File[/path/to/file]') }
+        end
       end
     end
   end
